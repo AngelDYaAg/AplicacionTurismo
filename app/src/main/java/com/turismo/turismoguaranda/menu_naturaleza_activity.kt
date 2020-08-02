9a9613aea3821b2cf68ced9097a837a2685bcb03
AngelDYaAg <angel_0930@hotmail.es>
2020-08-01 20:23:25 -0500

    menu naturaleza

---
+++ app/src/main/java/com/turismo/turismoguaranda/menu_naturaleza_activity.kt
@@ -0,0 +1,11 @@
+package com.turismo.turismoguaranda
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class menu_naturaleza_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_naturaleza_activity)
+    }
+}
